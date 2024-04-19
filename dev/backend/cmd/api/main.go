6c49666fdd1e2c90b54f23197cedf45e14b24888
skapskap <skaprange@gmail.com>
2024-04-19 17:18:50 -0300

    feat: add api middlewares and api flags

--- dev/backend/cmd/api/main.go
+++ dev/backend/cmd/api/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+type config struct {
+	port int
+}
+
 func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4869, "API server port")
+	flag.Parse()
+
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", cfg.port)))
 }
